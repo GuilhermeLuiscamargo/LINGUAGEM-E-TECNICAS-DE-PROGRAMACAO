ea44759f12e081b1570b25db2a13282ed402ef56
Guilherme Luis Camargo Neto <73392797+GuilhermeLuiscamargo@users.noreply.github.com>
2026-08-04 09:27:21 -0300

    Exercício da área do triangulo em C

---
+++ Ex-Triangulo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	
+	int altura,base;
+	float area;
+altura =10;
+base =3;
+area=(altura*base)/2;
+	printf("A area do triangulo quadrado de altura %d e de base %d e %f",altura,base,area);
+	
+	
+	return 0;
+}
